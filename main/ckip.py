68e89b183160b7244820428acb617303fcc20090
KevinPan0508 <k0922322268@gmail.com>
2021-01-14 16:09:27 +0800

    print cutting

--- main/ckip.py
+++ main/ckip.py
@@ -3,8 +3,9 @@
 from ckiptagger import WS, POS, NER
 import new_define
 
-trash_ws = ['聯絡','站內','內信','網址','坊','實業','實業坊','版規','內文','原文','處罰','水桶''https','imgur','批踢踢','踢踢','看板','文章','刪文','遵守','排版','作者','標題','時間','推','有','是']
-wanted_pos = ['Na','Nb','Nc','Nh','VC','VK','VJ','VG','VHC']
+trash_ws = ['本板','違反','規定','板務','水桶','檢舉','板規','編輯','我,','你','發信站','我們','板主','板主群','聯絡','站內','內信','網址','坊','實業','實業坊','版規','內文','原文','處罰','水桶''https','imgur','批踢踢','踢踢','看板','文章','刪文','遵守','排版','作者','標題','時間','推','有','是']
+wanted_pos = ['Na','Nb','Nc','VC','VK','VJ','VG','VHC']
+#remove Nh
 delimiter_set = {'，',',','。','.','：',':','[',']','「','」','！','!','、','(',')','/','-'}
 
 
@@ -32,6 +33,7 @@ def ckip_cut(list):
     pos_list = []
     ws_list = []
     final_list = []
+    print('Cutting')
     word_sentence_list = ws(list,sentence_segmentation=True,segment_delimiter_set=delimiter_set)
     pos_sentence_list = pos(word_sentence_list)
     for i in range(len(word_sentence_list)):
