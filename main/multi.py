9926a0292705ebaca03d51d51d2033497fc7ce74
KevinPan0508 <k0922322268@gmail.com>
2021-01-14 17:13:42 +0800

    multi-threading update

--- main/multi.py
+++ main/multi.py
@@ -1,8 +1,9 @@
 import new_define as nd
 import threading
 import ckip
+import queue
 from bs4 import BeautifulSoup
-import re
+import re   
 
 def multi_get_first_page_url(board):
     current_page_url = "http://ptt.cc/bbs/" + board + "/index.html"
@@ -16,14 +17,15 @@ def multi_get_seg(board):
     first_page_url = multi_get_first_page_url(board)
     pattern = re.compile(r'\d+')
     url_digit = pattern.findall(first_page_url)
-    worker1_start_url = first_page_url
+    worker1_start_url = first_page_url.lower()
     worker2_start_url = 'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-1) + '.html'
     worker3_start_url = 'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-2) + '.html'
     worker4_start_url = 'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-3) + '.html'
     worker5_start_url = 'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-4) + '.html'
     worker6_start_url = 'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-5) + '.html'
     worker7_start_url = 'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-6) + '.html'
-    return first_page_url ,worker1_start_url,worker2_start_url,worker3_start_url,worker4_start_url,worker5_start_url,worker6_start_url,worker7_start_url
+    end =  'http://ptt.cc/bbs/' + board +'/index' + str(int(url_digit[0])-7) + '.html'
+    return worker1_start_url, worker2_start_url, worker3_start_url, worker4_start_url, worker5_start_url, worker6_start_url, worker7_start_url, end
 
 def multi_get_text(start_url,stop_url):
     text_list = []
@@ -36,30 +38,31 @@ def multi_get_text(start_url,stop_url):
             text_list.append(text)
         url_digit = pattern.findall(start_url)
         start_url = start_url.replace(url_digit[0],str(int(url_digit[0])-1))
+    text_list = ckip.cut(text_list)
     return text_list
 
 def multi_start(board):
+    ret = []
+    my_queue = queue.Queue()
     threads = []
     start,first,second,third,fourth,fifth,sixth,seventh = multi_get_seg('board')
-    threads.append(threading.Thread(target = multi_get_text, args = (start,first)))
-    threads.append(threading.Thread(target = multi_get_text, args = (first,second)))
-    threads.append(threading.Thread(target = multi_get_text, args = (second,third)))
-    threads.append(threading.Thread(target = multi_get_text, args = (third,fourth)))
-    threads.append(threading.Thread(target = multi_get_text, args = (fourth,fifth)))
-    threads.append(threading.Thread(target = multi_get_text, args = (fifth,sixth)))
-    threads.append(threading.Thread(target = multi_get_text, args = (sixth,seventh)))
-    text1 = threads[0].start()
-    text2 = threads[1].start()
-    text3 = threads[2].start()
-    text4 = threads[3].start()
-    text5 = threads[4].start()
-    text6 = threads[5].start()
-    text7 = threads[6].start()
-    length = len(text1)
-    return text1,text2,text3,text4,text5,text6,text7,length
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,start,first)))
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,first,second)))
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,second,third)))
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,third,fourth)))
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,fourth,fifth)))
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,fifth,sixth)))    
+    threads.append(threading.Thread(target=lambda q, arg1, arg2: q.put(multi_get_text(arg1,arg2)), args=(my_queue,sixth,seventh)))
+    for i in range(7):
+        threads[i].start()
+    for i in range(7):
+        threads[i].join()
+    while not my_queue.empty():
+        ret.append(my_queue.get())
+    return ret[0],ret[1],ret[2],ret[3],ret[4],ret[5],ret[6]
 
-def multi_ckip_cut():
-    text1,text2,text3,text4,text5,text6,text7,length = multi_start()
+def multi_ckip_cut(board):
+    text1,text2,text3,text4,text5,text6,text7,length = multi_start(board)
     threads = []
     threads.append(threading.Thread(target = ckip.ckip_cut, args = (text1)))
     threads.append(threading.Thread(target = ckip.ckip_cut, args = (text2)))
@@ -76,3 +79,4 @@ def multi_ckip_cut():
     text6_cut = threads[5].start()
     text7_cut = threads[6].start()
     return text1_cut, text2_cut, text3_cut, text4_cut, text5_cut, text6_cut, text7_cut, length
+
